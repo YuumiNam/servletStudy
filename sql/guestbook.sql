299f0e676024fa071fb6ed412b1c487fd0f48235
dltjsam <dltjsam@gmail.com>
2022-11-07 20:09:21 +0900

    guestbook

--- sql/guestbook.sql
+++ sql/guestbook.sql
@@ -2,5 +2,12 @@ desc guestbook;
 
 insert into guestbook values(null, '둘리', '1234', '호이~', now());
 
-select no, name, contents, date_format(reg_date, '%Y/%m/%d %H:%i:%s')
+select no, name, contents, date_format(reg_date, '%Y/%m/%d %H:%i:%s') as date
 	from guestbook;
+    
+select * from guestbook;
+
+delete  
+	from guestbook
+    where no = '4'
+		and password = '1234';
