caa1bff0e42cd0c608b7c2f2d96c023628b4ba8c
dltjsam <dltjsam@gmail.com>
2022-11-08 14:26:30 +0900

    guestbook

---
+++ guestbook02/src/main/java/com/bitacademy/guestbook/controller/GuestbookController.java
@@ -0,0 +1,73 @@
+package com.bitacademy.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.guestbook.dao.GuestbookDao;
+import com.bitacademy.guestbook.vo.GuestbookVo;
+
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a"); // 파라미터 값을 임의로 설정해주는 것은 get방식으로써 나중에 url에 써줘야함
+												   // ex) http://localhost:8080/guestbook02/gb?a=list
+		
+//		// 메인화면은 else뒤로 빼줘야함. 이용자가 URL을 잘못입력했을시에도 메인화면에 들어올수 있게끔 해줘야하기 때문
+//		if("list".equals(action)) {
+//			List<GuestbookVo> list = new GuestbookDao().findAll();
+//			
+//			request.setAttribute("list", list);
+//			
+//			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/views/index.jsp");
+//			rd.forward(request, response);} 
+		if("add".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String contents = request.getParameter("contents");
+			
+			 GuestbookVo vo= new GuestbookVo();
+			 vo.setName(name);
+			 vo.setPassword(password);
+			 vo.setContents(contents);
+			 
+			 new GuestbookDao().insert(vo);
+			 
+			 response.sendRedirect("./gb");
+		} else if("deleteform".equals(action)) { 
+			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/views/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			String sno = request.getParameter("no");
+			Long no = Long.parseLong(sno);
+			String psw = request.getParameter("password");
+			
+			new GuestbookDao().deleteByNoAndPassword(no, psw);
+			
+			response.sendRedirect("./gb");
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+}
---
+++ guestbook02/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,55 @@
+<%@page import="java.util.List"%>
+<%@page import="com.bitacademy.guestbook.dao.GuestbookDao"%>
+<%@page import="com.bitacademy.guestbook.vo.GuestbookVo"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
+<%
+	List<GuestbookVo> list = (List<GuestbookVo>)request.getAttribute("list"); // down casting
+	int count = 1;
+%>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록</title>
+</head>
+
+<body>
+	<form action="./gb?a=add" method="post">
+	<table border=1 width=500>
+		<tr>
+			<td>이름</td><td><input type="text" name="name"></td>
+			<td>비밀번호</td><td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td colspan=4><textarea name= "contents" cols=60 rows=5></textarea></td>
+		</tr>
+		<tr>
+			<td colspan=4 align=right><input type="submit" value="제출"></td>
+		</tr>
+	</table>
+	</form>
+	<br>
+	
+	
+	<!-- 게스트 리스트 -->
+	<%
+		for(GuestbookVo vo : list) {
+	%>
+	<table width=510 border=1>
+		<tr>
+			<td>[<%=count++ %>]</td>
+			<td><%=vo.getName() %></td>
+			<td><%=vo.getDate() %></td>
+			<td><a href="./gb?a=deleteform&no=<%=vo.getNo() %>">삭제</a></td> <!-- no는 여기서 get방식으로 지정했음 -->
+		</tr>
+		<tr>
+			<td colspan=4><%=vo.getContents().replaceAll("\n", "<br>") %></td>
+		</tr>
+	</table>
+	<%
+		}
+	%>
+</body>
+</html>
